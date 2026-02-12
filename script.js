72de4139e6dc9264de164ff9b6aa13e90e97a475
siddhika-m <siddhika.mathur@innoraft.com>
2026-02-12 15:50:17 +0530

    updated code with comments

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
+//portfolio section image render funtion
 const mainImg = document.getElementById("mainImg");
 const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+const img3 = document.getElementById("img3");
+const img4 = document.getElementById("img4");
+
 img1.onclick = function () {
     mainImg.src = "images/image-product-1.jpg"
 }
-const img2 = document.getElementById("img2");
+
 img2.onclick = function () {
     mainImg.src = "images/image-product-2.jpg"
 }
-const img3 = document.getElementById("img3");
+
 img3.onclick = function () {
     mainImg.src = "images/image-product-3.jpg"
 }
-const img4 = document.getElementById("img4");
+
 img4.onclick = function () {
     mainImg.src = "images/image-product-4.jpg"
 }
 
+//add item plus-minus function
 let valueElement = document.getElementById("value");
 let minusBtn = document.getElementById("minus");
 let plusBtn = document.getElementById("plus");
@@ -24,11 +30,16 @@ let counterValue = 0;
 valueElement.value = counterValue;
 
 minusBtn.onclick = function () {
-    counterValue -= 1;
-    valueElement.value = counterValue; 
+    if (counterValue > 0) {
+        counterValue -= 1;
+        valueElement.value = counterValue;
+    }
+    else {
+        valueElement.value = 0;
+    }
 }
 
 plusBtn.onclick = function () {
     counterValue += 1;
-    valueElement.value = counterValue; 
+    valueElement.value = counterValue;
 }
