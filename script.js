0396ab1900a604f171b789a2ed8e8e18fc6eca2e
siddhika-m <siddhika.mathur@innoraft.com>
2026-02-17 11:54:14 +0530

    added slideshow functionality

--- script.js
+++ script.js
@@ -4,21 +4,32 @@ const img1 = document.getElementById("img1");
 const img2 = document.getElementById("img2");
 const img3 = document.getElementById("img3");
 const img4 = document.getElementById("img4");
+let mIndex = 1;
+
+const mImg1 = "images/image-product-1.jpg";
+const mImg2 = "images/image-product-2.jpg";
+const mImg3 = "images/image-product-3.jpg";
+const mImg4 = "images/image-product-4.jpg";
+
 
 img1.onclick = function () {
-    mainImg.src = "images/image-product-1.jpg"
+    mIndex = 1
+    mainImg.src = mImg1
 }
 
 img2.onclick = function () {
-    mainImg.src = "images/image-product-2.jpg"
+    mIndex = 2
+    mainImg.src = mImg2
 }
 
 img3.onclick = function () {
-    mainImg.src = "images/image-product-3.jpg"
+    mIndex = 3
+    mainImg.src = mImg3
 }
 
 img4.onclick = function () {
-    mainImg.src = "images/image-product-4.jpg"
+    mIndex = 4
+    mainImg.src = mImg4
 }
 
 //add item plus-minus function
@@ -42,4 +53,46 @@ minusBtn.onclick = function () {
 plusBtn.onclick = function () {
     counterValue += 1;
     valueElement.value = counterValue;
+}
+
+//adding functionality to the slide buttons
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+
+prevBtn.onclick = function () {
+    if (mIndex === 1) {
+        mainImg.src = mImg4
+        mIndex = 4
+    }
+    else if (mIndex === 2) {
+        mainImg.src = mImg1
+        mIndex = 1
+    }
+    else if (mIndex === 3) {
+        mainImg.src = mImg2
+        mIndex = 2
+    }
+    else if (mIndex === 4) {
+        mainImg.src = mImg3
+        mIndex = 3
+    }
+}
+
+nextBtn.onclick = function () {
+    if (mIndex === 1) {
+        mainImg.src = mImg2
+        mIndex = 2
+    }
+    else if (mIndex === 2) {
+        mainImg.src = mImg3
+        mIndex = 3
+    }
+    else if (mIndex === 3) {
+        mainImg.src = mImg4
+        mIndex = 4
+    }
+    else if (mIndex === 4) {
+        mainImg.src = mImg1
+        mIndex = 1
+    }
 }
